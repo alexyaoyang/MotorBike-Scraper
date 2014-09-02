84851e3959055350c465c2e17e0139b1bf835b8f
Alex Yao <alex@Alexs-MacBook-Pro-2.local>
2014-09-02 21:13:48 +0800

    get bike info from singaporebikes.com

--- scraper.py
+++ scraper.py
@@ -1,7 +1,32 @@
 from lxml import html
-import requests
+import requests, json
 page = requests.get('http://www.singaporebikes.com/index.php?main=used_bikes_listing&sub=adv_search&pgn=1&sort=&keyword=&makers_id=&availability_id=&vehclass_id[]=1&min_price=&max_price=&transmission_id=&year_manufactured=')
 tree = html.fromstring(page.text)
-#This will create a list of prices
-prices = tree.xpath('//td[@class="text text_red"]/text()')
-print 'Prices: ', prices
+url = tree.xpath('//a[@class="readmore_button_text"]/@href')
+bikes = []
+for link in url:
+    page = requests.get(link)
+    tree = html.fromstring(page.text)
+    image = tree.xpath('//a[@class="lightbox"][1]/@href')
+    #reg = tree.xpath('//td[@class="text_black"][5]/text()')
+    #expire = tree.xpath('//td[@class="text_black"][8]/text()')
+    #model = tree.xpath('//td[@class="text_black"][13]/text()')
+    #price = tree.xpath('//td[@class="text_black"][16]/text()')
+    #desc = tree.xpath('//td[@class="text_black"][22]/text()')
+    #name = tree.xpath('//td[@class="text_black"][23]/text()')
+    #contact = tree.xpath('//td[@class="text_black"][25]/text()')
+    info = tree.xpath('//td[@class="text_black"]/text()')
+    bike = {
+        "image":image[0],
+        #"reg":reg[0],
+        #"expire":expire[0],
+        #"model":model[0],
+        #"price":price[0],
+        #"desc":desc[0],
+        #"name":name[0],
+        #"contact":contact[0],
+        "info":info
+    }
+    bikes.append(json.dumps(bike))
+    
+print bikes[0]
