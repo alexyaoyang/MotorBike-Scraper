6b86fe04b6bb80baa627a7340f8d86410188a850
Alex Yao <alexyaoyang@hotmail.com>
2014-09-03 17:39:16 +0800

    calculate price/day

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from lxml import html
 import requests, json, webbrowser
+from datetime import datetime
 page = requests.get('http://www.singaporebikes.com/index.php?main=used_bikes_listing&sub=adv_search&pgn=1&sort=&keyword=&makers_id=&availability_id=&vehclass_id[]=1&min_price=&max_price=&transmission_id=&year_manufactured=')
 tree = html.fromstring(page.text)
 url = tree.xpath('//a[@class="readmore_button_text"]/@href')
@@ -15,8 +16,16 @@ for link in url:
     desc = tree.xpath('//td[@class="text_black"][22]/text()')
     name = tree.xpath('//td[@class="text_black"][23]/text()')
     contact = tree.xpath('//td[@class="text_black"][25]/text()')
+    ppd = 0
     href = link
-    #info = tree.xpath('//td[@class="text_black"]/text()')
+    newprice = price[0].replace('SGD$','').strip()
+    newprice = newprice.replace(',','')
+    if len(expire[0])>3 and len(newprice)<=5 and len(newprice)>0:
+        expiredate = datetime.strptime(expire[0],"%d-%m-%Y")
+        today = datetime.today()
+        days = (expiredate-today).days
+        ppd=float(newprice)/days
+        ppd = "{0:.2f}".format(ppd)
     bike = {
         "image":image[0],
         "reg":reg[0],
@@ -26,8 +35,8 @@ for link in url:
         "desc":desc[0],
         "name":name[0],
         "contact":contact[0],
-        "href":href
-        #"info":info
+        "href":href,
+        "ppd":'$'+ppd
     }
     bikes.append(bike)
     
