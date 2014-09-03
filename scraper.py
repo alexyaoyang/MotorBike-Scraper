fef21b1b6643598f433841653a5522d829c4feb0
Alex Yao <alexyaoyang@hotmail.com>
2014-09-03 23:27:46 +0800

    change default url

--- scraper.py
+++ scraper.py
@@ -91,5 +91,5 @@ for link in url:
 
 with open('bikes.json', 'w') as outfile:
   json.dump(bikes, outfile)
-webbrowser.open("file:///Users/alex/Documents/MotorBike%20Scraper/scraper.html",2)    
+webbrowser.open("file:///Users/alex/Documents/MotorBikeScraper/scraper.html",2)    
 print "done!"
