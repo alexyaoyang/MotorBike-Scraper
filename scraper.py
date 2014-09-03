eee24a11d913c8ab9ef618ee1a11931b4f59947c
Alex Yao <alexyaoyang@hotmail.com>
2014-09-03 09:37:46 +0800

    extra individual data

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 from lxml import html
-import requests, json
+import requests, json, webbrowser
 page = requests.get('http://www.singaporebikes.com/index.php?main=used_bikes_listing&sub=adv_search&pgn=1&sort=&keyword=&makers_id=&availability_id=&vehclass_id[]=1&min_price=&max_price=&transmission_id=&year_manufactured=')
 tree = html.fromstring(page.text)
 url = tree.xpath('//a[@class="readmore_button_text"]/@href')
@@ -8,28 +8,30 @@ for link in url:
     page = requests.get(link)
     tree = html.fromstring(page.text)
     image = tree.xpath('//a[@class="lightbox"][1]/@href')
-    #reg = tree.xpath('//td[@class="text_black"][5]/text()')
-    #expire = tree.xpath('//td[@class="text_black"][8]/text()')
-    #model = tree.xpath('//td[@class="text_black"][13]/text()')
-    #price = tree.xpath('//td[@class="text_black"][16]/text()')
-    #desc = tree.xpath('//td[@class="text_black"][22]/text()')
-    #name = tree.xpath('//td[@class="text_black"][23]/text()')
-    #contact = tree.xpath('//td[@class="text_black"][25]/text()')
-    info = tree.xpath('//td[@class="text_black"]/text()')
+    reg = tree.xpath('//td[@class="text_black"][5]/text()')
+    expire = tree.xpath('//td[@class="text_black"][8]/text()')
+    model = tree.xpath('//td[@class="text_black"][13]/text()')
+    price = tree.xpath('//td[@class="text_black"][16]/text()')
+    desc = tree.xpath('//td[@class="text_black"][22]/text()')
+    name = tree.xpath('//td[@class="text_black"][23]/text()')
+    contact = tree.xpath('//td[@class="text_black"][25]/text()')
+    href = link
+    #info = tree.xpath('//td[@class="text_black"]/text()')
     bike = {
         "image":image[0],
-        #"reg":reg[0],
-        #"expire":expire[0],
-        #"model":model[0],
-        #"price":price[0],
-        #"desc":desc[0],
-        #"name":name[0],
-        #"contact":contact[0],
-        "info":info
+        "reg":reg[0],
+        "expire":expire[0],
+        "model":model[0],
+        "price":price[0],
+        "desc":desc[0],
+        "name":name[0],
+        "contact":contact[0],
+        "href":href
+        #"info":info
     }
-    bikes.append(json.dumps(bike))
+    bikes.append(bike)
     
-print bikes[0]
-
-with open('bikes.txt','w') as outfile:
-    json.dump(bikes,outfile)
+with open('bikes.json', 'w') as outfile:
+  json.dump(bikes, outfile)
+webbrowser.open_new("scraper.html")    
+print "done!"
